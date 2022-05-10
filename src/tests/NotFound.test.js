b46515db21aadf51fda412175db2ed4e97d8499d
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-05-10 18:33:30 -0400

    Requirement 4 complete! :heavy_check_mark: NotFound test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Test the <NotFound.js /> component', () => {
+  it('Test if the page contains an h2 heading with the text "Page requested not found"',
+    () => {
+      renderWithRouter(<NotFound />);
+      const h2Heading = screen.getByRole(
+        'heading',
+        { name: 'Page requested not found Crying emoji' },
+        { level: 2 },
+      );
+      expect(h2Heading).toBeInTheDocument();
+    });
+  it('Test the page shows an image', () => {
+    renderWithRouter(<NotFound />);
+    const imageURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getByRole(
+      'img',
+      { name: 'Pikachu crying because the page requested was not found' },
+    );
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', imageURL);
+  });
+});
